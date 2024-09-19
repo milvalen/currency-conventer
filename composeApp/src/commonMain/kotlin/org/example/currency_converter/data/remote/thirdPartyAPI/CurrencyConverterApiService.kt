898a07396b6c3563bab61e5dc0429b67e393dab8
milvalen <artemyerkov@gmail.com>
2024-09-19 17:41:33 +0300

    api

---
+++ composeApp/src/commonMain/kotlin/org/example/currency_converter/data/remote/thirdPartyAPI/CurrencyConverterApiService.kt
@@ -0,0 +1,8 @@
+package org.example.currency_converter.data.remote.thirdPartyAPI
+
+class CurrencyConverterApiService {
+    companion object {
+        const val API_KEY = ""
+        const val API_ENDPOIND = ""
+    }
+}
